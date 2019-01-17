fdcdd932e5e9a937b8b6c37ed103526554d68164
Marco <mgenova03@gmail.com>
2019-01-17 12:12:35 -0500

    add role model and add specific roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
+
   namespace :api do
       namespace :v1 do
         get '/employees' => 'employees#index'
         post '/employees' => 'employees#create'
+
+        get '/roles' => 'roles#index'
       end
   end
 
