50a69b1e61d0e2060abbc03f3cc52d30231cc083
Marco <mgenova03@gmail.com>
2019-01-27 22:46:52 -0500

    add apolloClient

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 
-    post "/graphql", to: "graphql#execute"
+  post "/graphql", to: "graphql#execute"
 
   namespace :api do
       namespace :v1 do
